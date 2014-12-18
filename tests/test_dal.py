a598bf150dfdd1bfd0ada731a4c006a04fa08904
gi0baro <giovanni.barillari@gmail.com>
2014-12-18 01:29:27 +0100

    Fix in last commit, pt 2

--- tests/test_dal.py
+++ tests/test_dal.py
@@ -210,6 +210,7 @@ def test_callbacks(db):
 
 
 def test_virtualfields(db):
+    db.TModel._before_insert = []
     db.TModel._after_insert = []
     db.TModel.insert(a="foo", b="bar", price=12.95, quantity=3)
     db.commit()
