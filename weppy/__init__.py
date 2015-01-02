a22e357fcbcbed31856c369b79a2bfcdb0c248f7
gi0baro <giovanni.barillari@gmail.com>
2015-01-02 01:18:42 +0100

    Fixed init due to DAL update

--- weppy/__init__.py
+++ weppy/__init__.py
@@ -5,7 +5,7 @@ from .helpers import abort, stream_file
 from .tags import tag, cat, safe, asis
 from .forms import Form, DALForm
 from .cache import Cache
-from .dal import DAL, Field, ModelsDAL
+from .dal import DAL, Field
 from .globals import request, response, session
 from .handlers import Handler, Helper
 from .language import T
