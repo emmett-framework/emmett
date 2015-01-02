d891bf19d6ada31ef75074465de8a3ca7ebe5d7d
gi0baro <giovanni.barillari@gmail.com>
2015-01-02 18:19:28 +0100

    Added default validators to DAL

--- weppy/dal.py
+++ weppy/dal.py
@@ -12,10 +12,114 @@ import os
 from pydal import DAL as _pyDAL
 from pydal import Field
 from weppy import serializers as _serializers
+from weppy import validators as _validators
 from ._compat import copyreg
 from .datastructures import sdict
 from .handlers import Handler
 from .security import uuid as _uuid
+
+
+def _default_validators(db, field):
+    """
+    Field type validation, using web2py's validators mechanism.
+    makes sure the content of a field is in line with the declared
+    fieldtype
+    """
+
+    field_type, field_length = field.type, field.length
+    requires = []
+
+    def ff(r, id):
+        row = r(id)
+        if not row:
+            return str(id)
+        elif hasattr(r, '_format') and isinstance(r._format, str):
+            return r._format % row
+        elif hasattr(r, '_format') and callable(r._format):
+            return r._format(row)
+        else:
+            return str(id)
+
+    if field_type in (('string', 'text', 'password')):
+        requires.append(_validators.hasLength(field_length))
+    elif field_type == 'json':
+        requires.append(_validators.isEmptyOr(_validators.isJSON()))
+    elif field_type == 'double' or field_type == 'float':
+        requires.append(_validators.isFloatInRange(-1e100, 1e100))
+    elif field_type == 'integer':
+        requires.append(_validators.isIntInRange(-2**31, 2**31))
+    elif field_type == 'bigint':
+        requires.append(_validators.isIntInRange(-2**63, 2**63))
+    elif field_type.startswith('decimal'):
+        requires.append(_validators.isDecimalInRange(-10**10, 10**10))
+    elif field_type == 'date':
+        requires.append(_validators.isDate())
+    elif field_type == 'time':
+        requires.append(_validators.isTime())
+    elif field_type == 'datetime':
+        requires.append(_validators.isDatetime())
+    elif db and field_type.startswith('reference') and \
+            field_type.find('.') < 0 and \
+            field_type[10:] in db.tables:
+        referenced = db[field_type[10:]]
+
+        def repr_ref(id, row=None, r=referenced, f=ff):
+            return f(r, id)
+
+        field.represent = field.represent or repr_ref
+        if hasattr(referenced, '_format') and referenced._format:
+            requires = _validators.inDb(db, referenced._id, referenced._format)
+            if field.unique:
+                requires._and = _validators.notInDb(db, field)
+            if field.tablename == field_type[10:]:
+                return _validators.isEmptyOr(requires)
+            return requires
+    elif db and field_type.startswith('list:reference') and \
+            field_type.find('.') < 0 and \
+            field_type[15:] in db.tables:
+        referenced = db[field_type[15:]]
+
+        def list_ref_repr(ids, row=None, r=referenced, f=ff):
+            if not ids:
+                return None
+            from ..adapters.google import GoogleDatastoreAdapter
+            refs = None
+            db, id = r._db, r._id
+            if isinstance(db._adapter, GoogleDatastoreAdapter):
+                def count(values):
+                    return db(id.belongs(values)).select(id)
+                rx = range(0, len(ids), 30)
+                refs = reduce(lambda a, b: a & b, [count(ids[i:i+30])
+                              for i in rx])
+            else:
+                refs = db(id.belongs(ids)).select(id)
+            return (refs and ', '.join(f(r, x.id) for x in refs) or '')
+
+        field.represent = field.represent or list_ref_repr
+        if hasattr(referenced, '_format') and referenced._format:
+            requires = _validators.inDb(db, referenced._id, referenced._format,
+                                        multiple=True)
+        else:
+            requires = _validators.inDb(db, referenced._id, multiple=True)
+        if field.unique:
+            requires._and = _validators.notInDb(db, field)
+        if not field.notnull:
+            requires = _validators.isEmptyOr(requires)
+        return requires
+    elif field_type.startswith('list:'):
+        def repr_list(values, row=None):
+            return', '.join(str(v) for v in (values or []))
+
+        field.represent = field.represent or repr_list
+
+    if field.unique:
+        requires.append(_validators.notInDb(db, field))
+    sff = ['in', 'do', 'da', 'ti', 'de', 'bo']
+    if field.notnull and not field_type[:2] in sff:
+        requires.append(_validators.isntEmpty())
+    elif not field.notnull and field_type[:2] in sff and requires:
+        requires[0] = _validators.isEmptyOr(requires[0])
+    return requires
 
 
 class DALHandler(Handler):
@@ -36,7 +140,7 @@ class DALHandler(Handler):
 
 class DAL(_pyDAL):
     serializers = _serializers
-    #validators_method = _default_validators
+    validators_method = _default_validators
     logger = None
     uuid = _uuid
 
