eb888caf83435c5b48ba68c3be9a88141c990870
gi0baro <giovanni.barillari@gmail.com>
2015-01-07 15:09:23 +0100

    Added SessionFSManager to store sessions on filesystem

--- weppy/sessions.py
+++ weppy/sessions.py
@@ -9,7 +9,11 @@
     :license: BSD, see LICENSE for more details.
 """
 
-from ._compat import pickle
+import os
+import sys
+import time
+import tempfile
+from ._compat import PY2, pickle
 from .security import secure_loads, secure_dumps, uuid
 from .handlers import Handler
 from .globals import current, request, response
@@ -47,6 +51,105 @@ class SessionCookieManager(Handler):
         self.on_success()
 
 
+class SessionFSManager(Handler):
+    _fs_transaction_suffix = '.__wp_sess'
+    _fs_mode = 0o600
+
+    def __init__(self, expire=3600, secure=False, domain=None,
+                 filename_template='weppy_%s.sess'):
+        assert not filename_template.endswith(self._fs_transaction_suffix), \
+            'filename templates cannot end with %s' % \
+            self._fs_transaction_suffix
+        self._filename_template = filename_template
+        from .expose import Expose
+        self._path = os.path.join(Expose.application.root_path, 'sessions')
+        #: create required paths if needed
+        if not os.path.exists(self._path):
+            os.mkdir(self._path)
+        self.expire = expire
+        self.secure = secure
+        self.domain = domain
+
+    def _get_filename(self, sid):
+        if PY2 and isinstance(sid, unicode):
+            sid = sid.encode(sys.getfilesystemencoding() or 'utf-8')
+        return os.path.join(self._path, self._filename_template % sid)
+
+    def _load(self, sid):
+        try:
+            f = open(self._get_filename(sid), 'rb')
+        except IOError:
+            return None
+        now = time.time()
+        exp = pickle.load(f)
+        val = pickle.load(f)
+        f.close()
+        if exp < now:
+            f.close()
+            return None
+        return val
+
+    def _store(self, session, expiration):
+        fn = self._get_filename(session._sid)
+        now = time.time()
+        exp = now + expiration
+        fd, tmp = tempfile.mkstemp(suffix=self._fs_transaction_suffix,
+                                   dir=self._path)
+        f = os.fdopen(fd, 'wb')
+        try:
+            pickle.dump(exp, f, 1)
+            #f.write(session._dump)
+            pickle.dump(sdict(session), f, pickle.HIGHEST_PROTOCOL)
+        finally:
+            f.close()
+        try:
+            os.rename(tmp, fn)
+            os.chmod(fn, self._fs_mode)
+        except:
+            pass
+
+    def _delete(self, session):
+        fn = self._get_filename(session._sid)
+        try:
+            os.unlink(fn)
+        except OSError:
+            pass
+
+    def on_start(self):
+        self.cookie_data_name = 'wpp_session_data_%s' % request.application
+        if self.cookie_data_name in request.cookies:
+            sid = request.cookies[self.cookie_data_name].value
+            data = self._load(sid)
+            if data is not None:
+                current.session = SessionData(data, sid=sid)
+        if not current.session:
+            sid = uuid()
+            current.session = SessionData(sid=sid)
+
+    def on_success(self):
+        if not current.session:
+            self._delete(current.session)
+            if current.session._modified:
+                #: if we got here means we want to destroy session definitely
+                if self.cookie_data_name in response.cookies:
+                    del response.cookies[self.cookie_data_name]
+            return
+        #: store and update cookies
+        expiration = current.session._expiration or self.expire
+        if current.session._modified:
+            self._store(current.session, expiration)
+        response.cookies[self.cookie_data_name] = current.session._sid
+        response.cookies[self.cookie_data_name]['path'] = "/"
+        response.cookies[self.cookie_data_name]['expires'] = expiration
+        if self.secure:
+            response.cookies[self.cookie_data_name]['secure'] = True
+        if self.domain is not None:
+            response.cookies[self.cookie_data_name]['domain'] = self.domain
+
+    def on_failure(self):
+        self.on_success()
+
+
 class SessionRedisManager(Handler):
     def __init__(self, redis, prefix="wppsess:", expire=3600, secure=False,
                  domain=None):
@@ -60,7 +163,7 @@ class SessionRedisManager(Handler):
         self.cookie_data_name = 'wpp_session_data_%s' % request.application
         if self.cookie_data_name in request.cookies:
             sid = request.cookies[self.cookie_data_name].value
-            ## load from redis
+            #: load from redis
             data = self.redis.get(self.prefix+sid)
             if data is not None:
                 current.session = SessionData(pickle.loads(data), sid=sid)
@@ -72,11 +175,11 @@ class SessionRedisManager(Handler):
         if not current.session:
             self.redis.delete(self.prefix+current.session._sid)
             if current.session._modified:
-                ## if we got here means we want to destroy session definitely
+                #: if we got here means we want to destroy session definitely
                 if self.cookie_data_name in response.cookies:
                     del response.cookies[self.cookie_data_name]
             return
-        ## store on redis
+        #: store on redis and update cookies
         expiration = current.session._expiration or self.expire
         if current.session._modified:
             self.redis.setex(self.prefix + current.session._sid,
