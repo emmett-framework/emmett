007a78fa92d0fa98d1067d96871efaa81fc8681a
gi0baro <giovanni.barillari@gmail.com>
2015-08-09 13:46:28 +0200

    Releasing 0.4.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ Links
 from setuptools import setup
 setup(
     name='weppy',
-    version='0.4',
+    version='0.4.1',
     url='http://github.com/gi0baro/weppy/',
     license='BSD',
     author='Giovanni Barillari',
