emmett-framework/emmett

commit 45789ccb2799362f4b871b3971cf097b29140ee2

Using cachedprop decorator on lazy properties

weppy/globals.py
@@ -21,7 +21,11 @@ from ._internal import ObjectProxy, LimitedStream
 from .datastructures import sdict
 from .helpers import get_flashed_messages
 from .tags import htmlescape
+from .utils import cachedprop
 from .libs.contenttype import contenttype
+
+
+_regex_client = re.compile('[\w\-:]+(\.[\w\-]+)*\.?')
 
 
 class Request(object):
@@ -43,13 +47,21 @@ class Request(object):
         self.nowloc = environ['wpp.now.local']
         self.application = environ['wpp.application']
 
-    def _parse_get_vars(self):
+    @cachedprop
+    def now(self):
+        if self._now_ref == "utc":
+            return self.nowutc
+        return self.nowloc
+
+    @cachedprop
+    def get_vars(self):
         query_string = self.environ.get('QUERY_STRING', '')
         dget = cgi.parse_qs(query_string, keep_blank_values=1)
-        get_vars = self._get_vars = sdict(dget)
+        get_vars = sdict(dget)
         for key, value in iteritems(get_vars):
             if isinstance(value, list) and len(value) == 1:
                 get_vars[key] = value[0]
+        return get_vars
 
     def __parse_post_json(self):
         content_length = self.environ.get('CONTENT_LENGTH')
@@ -65,8 +77,9 @@ class Request(object):
             json_vars = {}
         return json_vars
 
-    def _parse_post_vars(self):
-        post_vars = self._post_vars = sdict()
+    @cachedprop
+    def post_vars(self):
+        post_vars = sdict()
         if self.environ.get('CONTENT_TYPE', '')[:16] == 'application/json':
             json_vars = self.__parse_post_json()
             post_vars.update(json_vars)
@@ -86,92 +99,58 @@ class Request(object):
                 dpk = [item.value if not item.filename else item
                        for item in dpk]
                 post_vars[key] = dpk
-            for key, value in iteritems(self._post_vars):
+            for key, value in list(post_vars.items()):
                 if isinstance(value, list) and len(value) == 1:
                     post_vars[key] = value[0]
+        return post_vars
 
-    def _parse_all_vars(self):
-        self._vars = copy.copy(self.get_vars)
+    @cachedprop
+    def vars(self):
+        rv = copy.copy(self.get_vars)
         for key, val in iteritems(self.post_vars):
-            if key not in self._vars:
-                self._vars[key] = val
+            if key not in rv:
+                rv[key] = val
             else:
-                if not isinstance(self._vars[key], list):
-                    self._vars[key] = [self._vars[key]]
-                self._vars[key] += val if isinstance(val, list) else [val]
+                if not isinstance(rv[key], list):
+                    rv[key] = [rv[key]]
+                rv[key] += val if isinstance(val, list) else [val]
 
-    def _parse_client(self):
-        regex_client = re.compile('[\w\-:]+(\.[\w\-]+)*\.?')
-        g = regex_client.search(self.environ.get('HTTP_X_FORWARDED_FOR', ''))
+    @cachedprop
+    def cookies(self):
+        cookies = SimpleCookie()
+        for cookie in self.environ.get('HTTP_COOKIE', '').split(';'):
+            cookies.load(cookie)
+        return cookies
+
+    @cachedprop
+    def client(self):
+        g = _regex_client.search(self.environ.get('HTTP_X_FORWARDED_FOR', ''))
         client = (g.group() or '').split(',')[0] if g else None
         if client in (None, '', 'unknown'):
-            g = regex_client.search(self.environ.get('REMOTE_ADDR', ''))
+            g = _regex_client.search(self.environ.get('REMOTE_ADDR', ''))
             if g:
                 client = g.group()
-            elif self.hostname.startswith('['):  # IPv6
+            elif self.hostname.startswith('['):
+                # IPv6
                 client = '::1'
             else:
-                client = '127.0.0.1'  # IPv4
+                # IPv4
+                client = '127.0.0.1'
         return client
 
-    @property
-    def now(self):
-        if self._now_ref == "utc":
-            return self.nowutc
-        return self.nowloc
-
-    @property
-    def get_vars(self):
-        " lazily parse the query string into get_vars "
-        if not hasattr(self, '_get_vars'):
-            self._parse_get_vars()
-        return self._get_vars
-
-    @property
-    def post_vars(self):
-        " lazily parse the request body into post_vars "
-        if not hasattr(self, '_post_vars'):
-            self._parse_post_vars()
-        return self._post_vars
-
-    @property
-    def vars(self):
-        " lazily parse the request body into post_vars "
-        if not hasattr(self, '_vars'):
-            self._parse_all_vars()
-        return self._vars
-
-    @property
-    def env(self):
-        """
-        lazily parse the environment variables into a sdict
-        """
-        if not hasattr(self, '_env'):
-            self._env = sdict(
-                (k.lower().replace('.', '_'), v)
-                for k, v in iteritems(self.environ)
-            )
-        return self._env
-
-    @property
-    def cookies(self):
-        " lazily parse the request cookies "
-        if not hasattr(self, '_cookies'):
-            self._cookies = SimpleCookie()
-            for cookie in self.environ.get('HTTP_COOKIE', '').split(';'):
-                self._cookies.load(cookie)
-        return self._cookies
-
-    @property
-    def client(self):
-        if not hasattr(self, '_client'):
-            self._client = self._parse_client()
-        return self._client
-
-    @property
+    @cachedprop
     def isajax(self):
         return self.environ.get('HTTP_X_REQUESTED_WITH', '').lower == \
             'xmlhttprequest'
+
+    @cachedprop
+    def env(self):
+        #: parse the environment variables into a sdict
+        _env = sdict(
+            (k.lower().replace('.', '_'), v)
+            for k, v in iteritems(self.environ)
+        )
+        return _env
 
     __getitem__ = object.__getattribute__
     __setitem__ = object.__setattr__
@@ -213,13 +192,10 @@ class Current(threading.local):
         self.session = None
         self._language = environ.get('HTTP_ACCEPT_LANGUAGE')
 
-    @property
+    @cachedprop
     def T(self):
-        #: lazily allocate translator (mainly for templates)
-        if not hasattr(self, '_t'):
-            from .language import T
-            self._t = T
-        return self._t
+        from .language import T
+        return T
 
 
 current = Current()

weppy/app.py
@@ -15,7 +15,7 @@ import click
 from yaml import load as ymlload
 from ._compat import basestring
 from ._internal import get_root_path, create_missing_app_folders
-from .utils import dict_to_sdict
+from .utils import dict_to_sdict, cachedprop
 from .expose import Expose
 from .datastructures import sdict, ConfigData
 from .wsgi import error_handler
@@ -69,22 +69,21 @@ class App(object):
         self.template_preloaders = {}
         self.template_lexers = {}
 
-    @property
+    @cachedprop
     def name(self):
         """The name of the application. This is usually the import name
         with the difference that it's guessed from the run file if the
         import name is main.
         """
-        if not hasattr(self, "_name"):
-            if self.import_name == '__main__':
-                fn = getattr(sys.modules['__main__'], '__file__', None)
-                if fn is None:
-                    self._name = '__main__'
-                else:
-                    self._name = os.path.splitext(os.path.basename(fn))[0]
+        if self.import_name == '__main__':
+            fn = getattr(sys.modules['__main__'], '__file__', None)
+            if fn is None:
+                rv = '__main__'
             else:
-                self._name = self.import_name
-        return self._name
+                rv = os.path.splitext(os.path.basename(fn))[0]
+        else:
+            rv = self.import_name
+        return rv
 
     @property
     def expose(self):

weppy/debug.py
@@ -20,6 +20,7 @@ import sys
 from types import TracebackType, CodeType
 from ._compat import PY2, reraise, iteritems
 from .templating.helpers import TemplateError
+from .utils import cachedprop
 
 
 # on pypy we can take advantage of transparent proxies
@@ -206,16 +207,14 @@ class Frame(object):
                 .replace("/", ".").split(".py")[0]
         return self.filename
 
-    @property
+    @cachedprop
     def sourcelines(self):
-        if not hasattr(self, '_sourcelines'):
-            try:
-                with open(self.filename, 'rb') as file:
-                    source = file.read().decode('utf8')
-            except IOError:
-                source = '<unavailable>'
-            self._sourcelines = source.splitlines()
-        return self._sourcelines
+        try:
+            with open(self.filename, 'rb') as file:
+                source = file.read().decode('utf8')
+        except IOError:
+            source = '<unavailable>'
+        return source.splitlines()
 
     @property
     def sourceblock(self):
@@ -240,16 +239,15 @@ class Frame(object):
         except IndexError:
             return u''
 
-    @property
+    @cachedprop
     def render_locals(self):
-        if not hasattr(self, '_rendered_locals'):
-            self._rendered_locals = dict()
-            for k, v in iteritems(self.locals):
-                try:
-                    self._rendered_locals[k] = str(v)
-                except:
-                    self._rendered_locals[k] = '<unavailable>'
-        return self._rendered_locals
+        rv = dict()
+        for k, v in iteritems(self.locals):
+            try:
+                rv[k] = str(v)
+            except:
+                rv[k] = '<unavailable>'
+        return rv
 
 
 def make_traceback(exc_info, template_ref=None):

weppy/forms.py
@@ -16,6 +16,7 @@ from .dal import Field
 from .datastructures import sdict
 from .globals import current, request, session
 from .tags import tag, TAG, cat, asis
+from .utils import cachedprop
 
 __all__ = ['Form', 'DALForm']
 
@@ -160,50 +161,49 @@ class Form(TAG):
             styler._add_hidden(key, value)
         return styler.render()
 
-    @property
+    @cachedprop
     def custom(self):
-        if not hasattr(self, '_custom'):
-            # init
-            self._custom = custom = sdict()
-            custom.dspval = sdict()
-            custom.inpval = sdict()
-            custom.label = sdict()
-            custom.comment = sdict()
-            custom.widget = sdict()
-            # load selected styler
-            styler = self.attributes['formstyle'](self.attributes)
-            styler.on_start()
-            # load data
-            for field in self.fields:
-                value = self.input_vars.get(field.name)
-                custom.dspval[field.name] = self.input_vars[field.name]
-                custom.inpval[field.name] = self.input_vars[field.name]
-                custom.label[field.name] = field.label
-                custom.comment[field.name] = field.comment
-                widget, wfield = styler._get_widget(field, value)
-                if not wfield:
-                    styler.style_widget(widget)
-                custom.widget[field.name] = widget
-            # add submit
-            custom.submit = tag.input(_type="submit",
-                                      value=self.attributes['submit'])
-            # provides begin attribute
-            begin = '<form action="%s" method="%s" enctype="%s">' % \
-                (self.attributes['_action'],
-                 self.attributes['_method'],
-                 self.attributes['_enctype'])
-            custom.begin = asis(begin)
-            # add hidden stuffs to get weppy process working
-            hidden = cat()
-            hidden.append(tag.input(_name='_csrf_token', _type='hidden',
-                                    _value=self.formkey))
-            for key, value in iteritems(self.attributes.get('hidden', {})):
-                hidden.append(tag.input(_name=key, _type='hidden',
-                              _value=value))
-            # provides end attribute
-            end = '%s</form>' % hidden.to_html()
-            custom.end = asis(end)
-        return self._custom
+        # init
+        custom = sdict()
+        custom.dspval = sdict()
+        custom.inpval = sdict()
+        custom.label = sdict()
+        custom.comment = sdict()
+        custom.widget = sdict()
+        # load selected styler
+        styler = self.attributes['formstyle'](self.attributes)
+        styler.on_start()
+        # load data
+        for field in self.fields:
+            value = self.input_vars.get(field.name)
+            custom.dspval[field.name] = self.input_vars[field.name]
+            custom.inpval[field.name] = self.input_vars[field.name]
+            custom.label[field.name] = field.label
+            custom.comment[field.name] = field.comment
+            widget, wfield = styler._get_widget(field, value)
+            if not wfield:
+                styler.style_widget(widget)
+            custom.widget[field.name] = widget
+        # add submit
+        custom.submit = tag.input(_type="submit",
+                                  value=self.attributes['submit'])
+        # provides begin attribute
+        begin = '<form action="%s" method="%s" enctype="%s">' % \
+            (self.attributes['_action'],
+             self.attributes['_method'],
+             self.attributes['_enctype'])
+        custom.begin = asis(begin)
+        # add hidden stuffs to get weppy process working
+        hidden = cat()
+        hidden.append(tag.input(_name='_csrf_token', _type='hidden',
+                                _value=self.formkey))
+        for key, value in iteritems(self.attributes.get('hidden', {})):
+            hidden.append(tag.input(_name=key, _type='hidden',
+                          _value=value))
+        # provides end attribute
+        end = '%s</form>' % hidden.to_html()
+        custom.end = asis(end)
+        return custom
 
     def to_html(self):
         return self._render().to_html()

weppy/language/__init__.py
@@ -8,13 +8,17 @@
     :license: BSD, see LICENSE for more details.
 """
 
+from ..utils import cachedprop
+
 
 class Instance(object):
+    @cachedprop
+    def _t(self):
+        from ..expose import Expose
+        from .translator import Translator
+        return Translator(Expose.application)
+
     def T(self, *args, **kwargs):
-        if not hasattr(self, "_t"):
-            from ..expose import Expose
-            from .translator import Translator
-            self._t = Translator(Expose.application)
         return self._t(*args, **kwargs)
 
 # We use a 'proxied' object to the translator to avoid errors

weppy/language/translator.py
@@ -19,6 +19,7 @@ import os
 from .._compat import PY2, implements_bool, implements_to_string, iteritems, \
     iterkeys, to_unicode
 from ..tags import asis, htmlescape
+from ..utils import cachedprop
 from .helpers import regex_backslash, regex_plural, regex_plural_dict, \
     regex_plural_tuple, regex_language, DEFAULT_NPLURALS, \
     DEFAULT_GET_PLURAL_ID, DEFAULT_CONSTRUCT_PLURAL_FORM, \
@@ -154,16 +155,20 @@ class TLanguage(object):
             self.pmtime = 0
             self.plural_file = None
 
+    def _read_t(self):
+        if self.filename is None:
+            return {}
+        return read_dict(self.filename)
+
+    @cachedprop
+    def _t(self):
+        return self._read_t()
+
     @property
     def t(self):
-        if not hasattr(self, "_t"):
-            if self.filename is None:
-                self._t = {}
-            else:
-                if not self.cached:
-                    return read_dict(self.filename)
-                self._t = read_dict(self.filename)
-        return self._t
+        if self.cached:
+            return self._t
+        return self._read_t()
 
     def set(self, key, message):
         # [WARN]: if it's not cached, is safe?
@@ -179,16 +184,20 @@ class TLanguage(object):
             return message
         return self.translator.default_translator.t.get(key, message)
 
+    def _read_pl(self):
+        if self.plural_file is None or self.pmtime == 0:
+            return {}
+        return read_plural_dict(self.plural_file)
+
+    @cachedprop
+    def _pl(self):
+        return self._read_pl()
+
     @property
     def pl(self):
-        if not hasattr(self, "_pl"):
-            if self.plural_file is None or self.pmtime == 0:
-                self._pl = {}
-            else:
-                if not self.cached:
-                    return read_plural_dict(self.plural_file)
-                self._pl = read_plural_dict(self.plural_file)
-        return self._pl
+        if self.cached:
+            return self._pl
+        return self._read_pl()
 
     def plural(self, word, n):
         """ get plural form of word for number *n*

weppy/utils.py
@@ -13,6 +13,20 @@ import re
 import socket
 
 
+class cachedprop(object):
+    #: a read-only @property that is only evaluated once.
+    def __init__(self, fget, doc=None):
+        self.fget = fget
+        self.__doc__ = doc or fget.__doc__
+        self.__name__ = fget.__name__
+
+    def __get__(self, obj, cls):
+        if obj is None:
+            return self
+        obj.__dict__[self.__name__] = result = self.fget(obj)
+        return result
+
+
 def is_valid_ip_address(address):
     REGEX_IPv4 = re.compile('(\d+)\.(\d+)\.(\d+)\.(\d+)')
     # deal with special cases

commit dfa513c469b9750fd97e65fbed1d43e58f518bad

Fixed bug introduced by 45789cc on request.vars

weppy/globals.py
@@ -114,6 +114,7 @@ class Request(object):
                 if not isinstance(rv[key], list):
                     rv[key] = [rv[key]]
                 rv[key] += val if isinstance(val, list) else [val]
+        return rv
 
     @cachedprop
     def cookies(self):