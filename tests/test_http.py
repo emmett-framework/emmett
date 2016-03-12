6afd9b2f042743b5cc1e5e890bfa0dfaeba70c74
vanadium23 <chernoffivan@gmail.com>
2016-03-12 21:50:02 +0300

    [Tests] start working on test for http

---
+++ tests/test_http.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+    tests.http
+    ----------------
+
+    Test weppy http module
+
+    :copyright: (c) 2014-2016 by Giovanni Barillari
+    :license: BSD, see LICENSE for more details.
+"""
+
+from weppy.http import HTTP
+
+
+def test_http():
+    response = []
+    buffer = []
+
+    def start_response(status, headers):
+        response[:] = [status, headers]
+        return buffer.append
+
+    http = HTTP(200, 'Hello World',
+                headers={'X-Test': 'Hello Header'},
+                cookies={'cookie_test': 'hello cookie'})
+
+    assert http.body == [b'Hello World']
+    assert http.status_code == 200
+    assert http.headers == [('X-Test', 'Hello Header'), ('Set-Cookie', 'e')]
+
+    assert http.to({'REQUEST_METHOD': 'HEAD'}, start_response) == [b'']
+    assert http.to({'REQUEST_METHOD': 'GET'}, start_response) == [b'Hello World']
