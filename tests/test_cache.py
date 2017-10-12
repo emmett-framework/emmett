7231f4ab41fe59048ec60dabd6d1d48a6ddf0e06
Giovanni Barillari <giovanni.barillari@gmail.com>
2017-10-12 19:11:02 +0200

    Refactored cache code, added decorator mode for cache, exposed `get`, `set`, `get_or_set`, `clear` methods in cache objects

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -10,16 +10,16 @@
 """
 
 
-from weppy.cache import BaseCache, RamCache, DiskCache, Cache
+from weppy.cache import CacheHandler, RamCache, DiskCache, Cache
 
 
 def test_basecache():
-    base_cache = BaseCache()
+    base_cache = CacheHandler()
     assert base_cache._default_expire == 300
 
-    assert base_cache('key') is None
-    assert base_cache._get('key') is None
-    assert base_cache._set('key', 'value', 300) is None
+    assert base_cache('key', lambda: 'value') is 'value'
+    assert base_cache.get('key') is None
+    assert base_cache.set('key', 'value', 300) is None
     assert base_cache.clear() is None
 
 
@@ -29,10 +29,16 @@ def test_ramcache():
     assert ram_cache._threshold == 500
 
     ram_cache('test', lambda: 2)
-    assert ram_cache._get('test') == 2
+    assert ram_cache.get('test') == 2
+
+    ram_cache.set('test', 3)
+    assert ram_cache.get('test') == 3
+
+    ram_cache.set('test', 4, 300)
+    assert ram_cache.get('test') == 4
 
     ram_cache.clear()
-    assert ram_cache._get('test') is None
+    assert ram_cache.get('test') is None
 
 
 def test_diskcache():
@@ -40,22 +46,61 @@ def test_diskcache():
     assert disk_cache._threshold == 500
 
     disk_cache('test', lambda: 2)
-    assert disk_cache._get('test') == 2
+    assert disk_cache.get('test') == 2
+
+    disk_cache.set('test', 3)
+    assert disk_cache.get('test') == 3
+
+    disk_cache.set('test', 4, 300)
+    assert disk_cache.get('test') == 4
 
     disk_cache.clear()
-    assert disk_cache._get('test') is None
+    assert disk_cache.get('test') is None
 
 
 def test_cache():
     default_cache = Cache()
-    assert default_cache.default_handler == 'ram'
+    assert isinstance(default_cache._default_handler, RamCache)
 
     default_cache('test', lambda: 2)
     assert default_cache('test', lambda: 3) == 2
+    default_cache.set('test', 3)
+    assert default_cache('test', lambda: 2) == 3
+    default_cache.set('test', 4, 300)
+    assert default_cache('test', lambda: 2, 300) == 4
+    default_cache.clear()
 
     disk_cache = DiskCache()
     ram_cache = RamCache()
     cache = Cache(default='disc', ram=ram_cache, disc=disk_cache)
-    assert cache.default_handler == 'disc'
+    assert isinstance(cache._default_handler, DiskCache)
     assert cache.disc == disk_cache
     assert cache.ram == ram_cache
+
+
+def test_cache_decorator():
+    cache = Cache(ram=RamCache(prefix='bar:'))
+
+    @cache('foo')
+    def foo(*args, **kwargs):
+        pass
+
+    #: no arguments
+    for _ in range(0, 2):
+        foo()
+    assert len(cache._default_handler.data.keys()) == 1
+    #: args change the cache key
+    for _ in range(0, 2):
+        foo(1)
+    assert len(cache._default_handler.data.keys()) == 2
+    for _ in range(0, 2):
+        foo(1, 2)
+    assert len(cache._default_handler.data.keys()) == 3
+    #: kwargs change the cache key
+    for _ in range(0, 2):
+        foo(1, a='foo', b='bar')
+    assert len(cache._default_handler.data.keys()) == 4
+    #: kwargs order won't change the cache key
+    for _ in range(0, 2):
+        foo(1, b='bar', a='foo')
+    assert len(cache._default_handler.data.keys()) == 4
