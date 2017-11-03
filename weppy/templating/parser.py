emmett-framework/emmett

commit a8a6db8ddb5d704e03b83dca3d2286092e1047dc

Avoid to change indentation in html <pre> blocks

weppy/templating/parser.py
@@ -15,7 +15,7 @@ import uuid
 from collections import namedtuple
 from .contents import (
     Node, NodeGroup, WriterNode, EscapeNode, HTMLNode, PrettyWriterNode,
-    PrettyEscapeNode, PrettyHTMLNode)
+    PrettyEscapeNode, PrettyHTMLNode, PrettyHTMLPreNode)
 from .helpers import TemplateError
 from .lexers import default_lexers
 
@@ -109,7 +109,7 @@ class ParsingState(object):
 class ParsingContext(object):
     def __init__(
         self, parser, name, text, scope, writer_node_cls, escape_node_cls,
-        html_node_cls
+        html_node_cls, htmlpre_node_cls
     ):
         self.parser = parser
         self.stack = []
@@ -122,6 +122,8 @@ class ParsingContext(object):
         self._writer_node_cls = writer_node_cls
         self._escape_node_cls = escape_node_cls
         self._html_node_cls = html_node_cls
+        self._htmlpre_node_cls = htmlpre_node_cls
+        self._in_html_pre = False
 
     @property
     def name(self):
@@ -205,9 +207,12 @@ class ParsingContext(object):
         self.content.append(node)
         return node
 
-    def html(self, value, indent=None):
+    def html(self, value):
+        node_cls = (
+            self._html_node_cls if not self._in_html_pre else
+            self._htmlpre_node_cls)
         self.content.append(
-            self._html_node_cls(
+            node_cls(
                 value, indent=self.state.indent, new_line=self.state.new_line,
                 source=self.state.source, lines=self.state.lines))
 
@@ -222,7 +227,9 @@ class ParsingContext(object):
 
 
 class TemplateParser(object):
-    _nodes_cls = {'writer': WriterNode, 'escape': EscapeNode, 'html': HTMLNode}
+    _nodes_cls = {
+        'writer': WriterNode, 'escape': EscapeNode, 'html': HTMLNode,
+        'htmlpre': HTMLNode}
 
     r_multiline = re.compile(r'(""".*?""")|(\'\'\'.*?\'\'\')', re.DOTALL)
 
@@ -280,19 +287,42 @@ class TemplateParser(object):
         return filename, file_path, text
 
     @staticmethod
-    def _cleanup_html_line(line, pos):
-        return line if not pos else line.lstrip()
+    def _check_html_pre(ctx, line):
+        if not ctx._in_html_pre and '<pre' in line and '</pre>' not in line:
+            return True, False
+        if ctx._in_html_pre and '</pre>' in line:
+            return False, True
+        return False, False
+
+    @staticmethod
+    def _start_html_pre(ctx, start):
+        if start:
+            ctx._in_html_pre = True
+
+    @staticmethod
+    def _end_html_pre(ctx, end):
+        if end:
+            ctx._in_html_pre = False
+
+    @staticmethod
+    def _cleanup_html_line(ctx, line, pos):
+        if pos and not ctx._in_html_pre:
+            return line.strip()
+        return line
 
     def parse_html_block(self, ctx, element):
         lines = element.split("\n")
         ctx.update_lines_count(len(lines) - 1)
         new_lines = []
         for _, line in enumerate(lines):
-            line = self._cleanup_html_line(line, _)
+            start_pre, end_pre = self._check_html_pre(ctx, line)
+            self._end_html_pre(ctx, end_pre)
+            line = self._cleanup_html_line(ctx, line, _)
             if line:
                 new_lines.append(line)
+            self._start_html_pre(ctx, start_pre)
         if new_lines:
-            ctx.html(''.join(new_lines))
+            ctx.html('\n'.join(new_lines))
 
     def _get_python_block_text(self, element):
         return element[self.delimiters_len[0]:-self.delimiters_len[1]].strip()
@@ -333,7 +363,8 @@ class TemplateParser(object):
     def _build_ctx(self, text):
         return ParsingContext(
             self, self.name, text, self.scope, self._nodes_cls['writer'],
-            self._nodes_cls['escape'], self._nodes_cls['html'])
+            self._nodes_cls['escape'], self._nodes_cls['html'],
+            self._nodes_cls['htmlpre'])
 
     def parse(self, text):
         ctx = self._build_ctx(text)
@@ -386,7 +417,8 @@ class PrettyTemplateParser(TemplateParser):
     _nodes_cls = {
         'writer': PrettyWriterNode,
         'escape': PrettyEscapeNode,
-        'html': PrettyHTMLNode}
+        'html': PrettyHTMLNode,
+        'htmlpre': PrettyHTMLPreNode}
 
     r_wspace = re.compile("^( *)")
 
@@ -406,10 +438,13 @@ class PrettyTemplateParser(TemplateParser):
         for line in lines:
             empty_line = not line
             indent = len(self.r_wspace.search(line).group(0))
+            start_pre, end_pre = self._check_html_pre(ctx, line)
+            self._end_html_pre(ctx, end_pre)
             line = line[indent:]
             ctx.state.indent = indent
             if line or empty_line:
                 ctx.html(line)
+            self._start_html_pre(ctx, start_pre)
             ctx.state.new_line = True
         #: set correct `new_line` state depending on last line
         if line and not removed_last_line:

weppy/templating/contents.py
@@ -110,3 +110,11 @@ class PrettyEscapeNode(PrettyMixin, EscapeNode):
 
 class PrettyHTMLNode(PrettyMixin, HTMLNode):
     pass
+
+
+class PrettyHTMLPreNode(PrettyHTMLNode):
+    def increment_indent(self, increment):
+        return
+
+    def change_indent(self, indent):
+        return

commit 68a733c714799b569fdf92da7bc1e5c0631cd129

Fixed typo in template parser introduced with a8a6db8

weppy/templating/parser.py
@@ -309,7 +309,7 @@ class TemplateParser(object):
     @staticmethod
     def _cleanup_html_line(ctx, line, pos):
         if pos and not ctx._in_html_pre:
-            return line.strip()
+            return line.lstrip()
         return line
 
     def parse_html_block(self, ctx, element):