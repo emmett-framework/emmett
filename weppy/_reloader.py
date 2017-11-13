c554c794e69b468e74853fbf1abe1c532fd1bfd4
Giovanni Barillari <giovanni.barillari@gmail.com>
2017-11-13 11:17:55 +0100

    Fixed reloader on windows

--- weppy/_reloader.py
+++ weppy/_reloader.py
@@ -24,92 +24,42 @@ from itertools import chain
 from ._compat import PY2, iteritems
 
 
-def _iter_module_files(reloader_paths=None):
+def _iter_module_files():
     """This iterates over all relevant Python files.  It goes through all
     loaded files from modules, all files in folders of already loaded modules
     as well as all files reachable through a package.
     """
-    found = set()
-    entered = set()
-
-    def _verify_file(filename):
-        if not filename:
-            return
-        filename = os.path.abspath(filename)
-        old = None
-        while not os.path.isfile(filename):
-            old = filename
-            filename = os.path.dirname(filename)
-            if filename == old:
-                break
-        else:
-            if filename[-4:] in ('.pyc', '.pyo'):
-                filename = filename[:-1]
-            if filename not in found:
-                found.add(filename)
-                return filename
-
-    def _recursive_walk(path_entry):
-        if path_entry in entered:
-            return
-        entered.add(path_entry)
-        try:
-            for filename in os.listdir(path_entry):
-                path = os.path.join(path_entry, filename)
-                if os.path.isdir(path):
-                    for filename in _recursive_walk(path):
-                        yield filename
-                else:
-                    if not filename.endswith(('.py', '.pyc', '.pyo')):
-                        continue
-                    filename = _verify_file(path)
-                    if filename:
-                        yield filename
-        except OSError:
-            pass
-
-    if not reloader_paths:
-        # The list call is necessary on Python 3 in case the module
-        # dictionary modifies during iteration.
-        for path_entry in list(sys.path):
-            for filename in _recursive_walk(os.path.abspath(path_entry)):
-                yield filename
-
-        for module in list(sys.modules.values()):
-            if module is None:
-                continue
-            filename = _verify_file(getattr(module, '__file__', None))
-            if filename:
-                yield filename
-                for filename in _recursive_walk(os.path.dirname(filename)):
-                    yield filename
-            for package_path in getattr(module, '__path__', ()):
-                for filename in _recursive_walk(os.path.abspath(package_path)):
-                    yield filename
-    else:
-        for path_entry in reloader_paths:
-            for filename in _recursive_walk(os.path.abspath(path_entry)):
+    for module in list(sys.modules.values()):
+        if module is None:
+            continue
+        filename = getattr(module, '__file__', None)
+        if filename:
+            old = None
+            while not os.path.isfile(filename):
+                old = filename
+                filename = os.path.dirname(filename)
+                if filename == old:
+                    break
+            else:
+                if filename[-4:] in ('.pyc', '.pyo'):
+                    filename = filename[:-1]
                 yield filename
 
 
-def _find_common_roots(paths):
-    """Out of some paths it finds the common roots that need monitoring."""
-    paths = [x.split(os.path.sep) for x in paths]
-    root = {}
-    for chunks in sorted(paths, key=len, reverse=True):
-        node = root
-        for chunk in chunks:
-            node = node.setdefault(chunk, {})
-        node.clear()
-
-    rv = set()
-
-    def _walk(node, path):
-        for prefix, child in iteritems(node):
-            _walk(child, path + (prefix,))
-        if not node:
-            rv.add('/'.join(path))
-    _walk(root, ())
+def _get_args_for_reloading():
+    """Returns the executable. This contains a workaround for windows
+    if the executable is incorrectly reported to not have the .exe
+    extension which can cause bugs on reloading.
+    """
+    rv = [sys.executable]
+    py_script = sys.argv[0]
+    if (
+        os.name == 'nt' and not os.path.exists(py_script) and
+        os.path.exists(py_script + '.exe')
+    ):
+        py_script += '.exe'
+    rv.append(py_script)
+    rv.extend(sys.argv[1:])
     return rv
 
 
@@ -121,10 +71,9 @@ class ReloaderLoop(object):
     # `eventlet.monkey_patch`) before we get here
     _sleep = staticmethod(time.sleep)
 
-    def __init__(self, reloader_paths=None, extra_files=None, interval=1):
-        self.reloader_paths = reloader_paths
-        self.extra_files = set(os.path.abspath(x)
-                               for x in extra_files or ())
+    def __init__(self, extra_files=None, interval=1):
+        self.extra_files = set(
+            os.path.abspath(x) for x in extra_files or ())
         self.interval = interval
 
     def run(self):
@@ -135,9 +84,8 @@ class ReloaderLoop(object):
         but running the reloader thread.
         """
         while 1:
-            #self.log('> Restarting (%s mode)' % self.name)
             print('> Restarting (%s mode)' % self.name)
-            args = [sys.executable] + sys.argv
+            args = _get_args_for_reloading()
             new_environ = os.environ.copy()
             new_environ['WEPPY_RUN_MAIN'] = 'true'
 
@@ -155,7 +103,6 @@ class ReloaderLoop(object):
 
     def trigger_reload(self, filename):
         filename = os.path.abspath(filename)
-        #self.log('> Detected change in %r, reloading' % filename)
         print('> Detected change in %r, reloading' % filename)
         sys.exit(3)
 
@@ -166,8 +113,7 @@ class StatReloaderLoop(ReloaderLoop):
     def run(self):
         mtimes = {}
         while 1:
-            for filename in chain(_iter_module_files(self.reloader_paths),
-                                  self.extra_files):
+            for filename in chain(_iter_module_files(), self.extra_files):
                 try:
                     mtime = os.stat(filename).st_mtime
                 except OSError:
@@ -189,12 +135,12 @@ reloader_loops = {
 reloader_loops['auto'] = reloader_loops['stat']
 
 
-def run_with_reloader(app, host, port, extra_files=None, interval=1,
-                      reloader_type='auto', reloader_paths=None):
+def run_with_reloader(
+    app, host, port, extra_files=None, interval=1, reloader_type='auto'
+):
     """Run the given function in an independent python interpreter."""
     import signal
-    reloader = reloader_loops[reloader_type](reloader_paths, extra_files,
-                                             interval)
+    reloader = reloader_loops[reloader_type](extra_files, interval)
     signal.signal(signal.SIGTERM, lambda *args: sys.exit(0))
     try:
         if os.environ.get('WEPPY_RUN_MAIN') == 'true':
