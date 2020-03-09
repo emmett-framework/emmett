daccd0fdf48e69c6cece05933cbba64068a0ff81
Giovanni Barillari <giovanni.barillari@gmail.com>
2020-03-09 18:11:01 +0100

    Updated basic context for template response builders

--- emmett/routing/response.py
+++ emmett/routing/response.py
@@ -50,20 +50,17 @@ class BytesResponseBuilder(ResponseBuilder):
 class TemplateResponseBuilder(ResponseProcessor):
     def process(self, output: Union[Dict[str, Any], None]) -> str:
         current.response.headers._data['content-type'] = _html_content_type
-        if output is None:
-            output = {
-                'current': current, 'url': url, 'asis': asis,
-                'load_component': load_component
-            }
-        else:
-            output['current'] = output.get('current', current)
-            output['url'] = output.get('url', url)
-            output['asis'] = output.get('asis', asis)
-            output['load_component'] = output.get(
-                'load_component', load_component)
+        base_ctx = {
+            'current': current,
+            'url': url,
+            'asis': asis,
+            'load_component': load_component
+        }
+        output = base_ctx if output is None else {**base_ctx, **output}
         try:
             return self.route.app.templater.render(
-                self.route.template, output)
+                self.route.template, output
+            )
         except TemplateMissingError as exc:
             raise HTTP(404, body="{}\n".format(exc.message))
 
@@ -73,22 +70,28 @@ class AutoResponseBuilder(ResponseProcessor):
         is_template = False
         if isinstance(output, dict):
             is_template = True
-            output['current'] = output.get('current', current)
-            output['url'] = output.get('url', url)
-            output['asis'] = output.get('asis', asis)
-            output['load_component'] = output.get(
-                'load_component', load_component)
-        elif output is None:
-            is_template = True
             output = {
-                'current': current, 'url': url, 'asis': asis,
+                **{
+                    'current': current,
+                    'url': url,
+                    'asis': asis,
+                    'load_component': load_component
+                },
+                **output
+            }
+        elif output is None:
+            output = {
+                'current': current,
+                'url': url,
+                'asis': asis,
                 'load_component': load_component
             }
         if is_template:
             current.response.headers._data['content-type'] = _html_content_type
             try:
                 return self.route.app.templater.render(
-                    self.route.template, output)
+                    self.route.template, output
+                )
             except TemplateMissingError as exc:
                 raise HTTP(404, body="{}\n".format(exc.message))
         elif isinstance(output, str):
