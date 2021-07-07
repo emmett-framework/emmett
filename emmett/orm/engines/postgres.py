8ede4cf1b1150e00a33f838f4cd2bce13f91292a
Giovanni Barillari <giovanni.barillari@gmail.com>
2021-07-07 18:16:16 +0200

    Update jsonb operators behavior (#357)

--- emmett/orm/engines/postgres.py
+++ emmett/orm/engines/postgres.py
@@ -17,7 +17,7 @@ from pydal.adapters.postgres import (
 from pydal.dialects import register_expression, sqltype_for
 from pydal.dialects.postgre import PostgreDialectBooleanJSON
 from pydal.helpers.serializers import serializers
-from pydal.objects import Expression
+from pydal.objects import Expression, Query
 from pydal.parsers import for_type as parse_type
 from pydal.parsers.postgre import PostgreBooleanAutoJSONParser
 from pydal.representers import for_type as repr_type
@@ -39,7 +39,7 @@ class JSONBPostgreDialect(PostgreDialectBooleanJSON):
 
     @register_expression('jcontains')
     def _jcontains_expr(self, expr, data):
-        return Expression(expr.db, self._jcontains, expr, data, expr.type)
+        return Query(expr.db, self._jcontains, expr, data)
 
     def _jin(self, field, data, query_env={}):
         return '(%s <@ %s)' % (
@@ -49,21 +49,27 @@ class JSONBPostgreDialect(PostgreDialectBooleanJSON):
 
     @register_expression('jin')
     def _jin_expr(self, expr, data):
-        return Expression(expr.db, self._jin, expr, data, expr.type)
+        return Query(expr.db, self._jin, expr, data)
+
+    def _jget_common(self, op, field, data, query_env):
+        if not isinstance(data, int):
+            _dtype = field.type if isinstance(data, (dict, list)) else 'string'
+            data = self.expand(data, field_type=_dtype, query_env=query_env)
+        return '%s %s %s' % (
+            self.expand(field, query_env=query_env),
+            op,
+            str(data)
+        )
 
     def _jget(self, field, data, query_env={}):
-        if not isinstance(data, int):
-            data = self.expand(data, field_type='string', query_env=query_env)
-        return '%s -> %s' % (self.expand(field, query_env=query_env), str(data))
+        return self._jget_common('->', field, data, query_env=query_env)
 
     @register_expression('jget')
     def _jget_expr(self, expr, data):
         return Expression(expr.db, self._jget, expr, data, expr.type)
 
     def _jgetv(self, field, data, query_env={}):
-        if not isinstance(data, int):
-            data = self.expand(data, field_type='string', query_env=query_env)
-        return '%s ->> %s' % (self.expand(field, query_env=query_env), str(data))
+        return self._jget_common('->>', field, data, query_env=query_env)
 
     @register_expression('jgetv')
     def _jgetv_expr(self, expr, data):
@@ -89,15 +95,20 @@ class JSONBPostgreDialect(PostgreDialectBooleanJSON):
     def _jpathv_expr(self, expr, data):
         return Expression(expr.db, self._jpathv, expr, data, 'string')
 
-    def _jhas(self, field, data, query_env={}):
-        return '%s ? %s' % (
+    def _jhas(self, field, data, all=False, query_env={}):
+        _op, _ftype = '?', 'string'
+        if isinstance(data, list):
+            _op = '?&' if all else '?|'
+            _ftype = 'list:string'
+        return '%s %s %s' % (
             self.expand(field, query_env=query_env),
-            self.expand(data, field_type='string', query_env=query_env)
+            _op,
+            self.expand(data, field_type=_ftype, query_env=query_env)
         )
 
     @register_expression('jhas')
-    def _jhas_expr(self, expr, data):
-        return Expression(expr.db, self._jhas, expr, data, 'boolean')
+    def _jhas_expr(self, expr, data, all=False):
+        return Query(expr.db, self._jhas, expr, data, all=all)
 
 
 class JSONBPostgreParser(PostgreBooleanAutoJSONParser):
